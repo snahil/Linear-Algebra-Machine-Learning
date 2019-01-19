cee6a874a0e4e795a5c4f43c02156d136b80271d
snahilindoria <snahilindoria999@gmail.com>
2019-01-19 11:19:33 +0530

    commit

--- Linear_Algebra.py
+++ Linear_Algebra.py
@@ -1,13 +1,28 @@
 import pandas as pd # we are giving name of panda library as pd
-form sklearn.linearmodelform sklearn.linearmodel #we are importing  a specfic package form the library for opt the code
+from sklearn.linear_model import LinearRegression #we are importing  a specfic package form the library for opt the code
 package from the 
+import matplotlib.pyplot as plt  # library for ploting graph
 
 dataset=pd.read_csv('marks.csv')
 
+
 y =  dataset.iloc[:,-1]   # The iolic indexer for pandas is used for integer based location
-x = dataset.iloc[:,0:1]
+
+X = dataset.iloc[:, 0:1] # x will be always in captial 
+
 
 model = LinearRegression()
 
+model.fit(X,y)
+
 
 model.fit
+
+model.predict([[7]])
+
+
+
+plt.scatter(X, y) # ploating Graph in 1 DeprecationWarning
+
+
+plt.plot(X,y) # Graph ploating in 2D 
